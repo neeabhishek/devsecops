009ad73a41b8e2b8517afdfc248f340168a83cb3
Abhishek Neelakandan <64977046+neeabhishek@users.noreply.github.com>
2026-05-16 19:19:35 +0530

    Update sastAnalysis.groovy

--- Pipeline/vars/sastAnalysis.groovy
+++ Pipeline/vars/sastAnalysis.groovy
@@ -8,7 +8,28 @@ def call(Map config = [:]){
         error("SonarQube Cred were not provided")
     }
 
-    withSonarQubeEnv(sonarQubeCred){
+    withSonarQubeEnv(def call(Map config = [:]){
+
+    def workspace = config.workspace ?: env.WORKSPACE
+    def serviceName = config.serviceName ?: env.SERVICE_NAME
+    def sonarQubeCred = config.sonarQubeCred 
+
+    if(!sonarQubeCred){
+        error("SonarQube Cred were not provided")
+    }
+
+    withSonarQubeEnv('SonarQubeInstance'){
+        def sast = sh (
+        script: """
+            cd ${workspace} && \
+            mvn sonar:sonar \
+            -Dsonar.projectName=${serviceName}
+        """,
+        returnStatus: true
+        )
+        return sast
+    }    
+}){
         def sast = sh (
         script: """
             cd ${workspace} && \
